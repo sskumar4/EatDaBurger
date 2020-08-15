54d88cb0e3963983b1515f866cbee8dc4befe40e
Sudha SK <sskumar4@ncsu.edu>
2020-08-15 08:33:02 -0400

    Make devour it! work

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -31,7 +31,7 @@ router.put("/api/burgers/:id", function(req, res) {
     console.log("condition", condition);
 
     burger.update({
-        sleepy: req.body.sleepy
+        devoured: req.body.devoured,
     }, condition, function(result) {
         if (result.changedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,11 +1,11 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".change-devoured").on("click", function(event) {
+    $(".devour-burger").on("click", function(event) {
         var id = $(this).data("id");
-        var newDevoured = $(this).data("newdevoured");
-
+        var newdevoured = $(this).data("newdevour");
+        console.log("newdevoured:::", newdevoured);
         var newDevouredState = {
-            devoured: newDevoured
+            devoured: newdevoured,
         };
 
         // Send the PUT request.
@@ -14,7 +14,7 @@ $(function() {
             data: newDevouredState
         }).then(
             function() {
-                console.log("changed devoured to", newDevoured);
+                console.log("changed devoured to", newdevoured);
                 // Reload the page to get the updated list
                 location.reload();
             }
